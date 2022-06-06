d83f06134650d89a32d3db91e6d68d1eceac9247
Fb185 <fredbobonejr@gmail.com>
2022-06-06 15:10:50 +0100

    commit

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,23 @@
 package controller;
 
+import controller.Controller;
+import models.*;
+
 public class Controller {
+
+  public AutocarroExpresso createAutocarroExpresso() {
+    AutocarroExpresso newExpresso = new AutocarroExpresso();
+  }
+
+  AutocarroConvencional newConvencional = new AutocarroConvencional();
+  AutocarroLongDrive newLongDrive = new AutocarroLongDrive();
+  AutocarroMiniBus newMiniBus = new AutocarroMiniBus();
+  Porto porto = new Porto();
+  Lisboa lisboa = new Lisboa();
+  Cascais cascais = new Cascais();
+  Coimbra coimbra = new Coimbra();
+  Braga braga = new Braga();
+
   public Controller() {
     System.out.println("Controller created");
   }
--- src/view/CLI.java
+++ src/view/CLI.java
@@ -1,19 +1,11 @@
 package view;
 
-import controller.Controller;
-import models.*;
+import controller.controller;
 
 public class CLI {
   public static void main(String[] args) {
-    Controller newcontroller = new Controller();
-    AutocarroConvencional newConvencional = new AutocarroConvencional();
-    AutocarroExpresso newExpresso = new AutocarroExpresso();
-    AutocarroLongDrive newLongDrive = new AutocarroLongDrive();
-    AutocarroMiniBus newMiniBus = new AutocarroMiniBus();
-    Porto porto = new Porto();
-    Lisboa lisboa = new Lisboa();
-    Cascais cascais = new Cascais();
-    Coimbra coimbra = new Coimbra();
-    Braga braga = new Braga();
+    // based on random parameters call creating functions
+    controller.createAutocarroExpresso();
+
   }
 }
